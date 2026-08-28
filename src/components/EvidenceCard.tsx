796a5f1fe979d85d2b39adbb06c0c8045c11b518
Juan Manuel Torres <juan.manuel.torres@mi.unc.edu.ar>
2026-08-28 16:42:28 -0300

    test: establish semantic EvidenceCard red

---
+++ src/components/EvidenceCard.tsx
@@ -0,0 +1,9 @@
+import type { TerritorialEvidence } from '../types/evidence'
+
+interface EvidenceCardProps {
+  evidence: TerritorialEvidence
+}
+
+export function EvidenceCard({ evidence: _evidence }: EvidenceCardProps) {
+  return <article>Not implemented</article>
+}
