4c943326d6951f6d0f18464b54f59d1b18f87fe0
Juan Manuel Torres <juan.manuel.torres@mi.unc.edu.ar>
2026-08-29 00:58:19 -0300

    feat: encode earthquake depth as color

---
+++ src/lib/earthquakeDepthScale.ts
@@ -0,0 +1,28 @@
+import type { ExpressionSpecification } from 'maplibre-gl'
+
+export const EARTHQUAKE_DEPTH_UNKNOWN_COLOR = '#8896b4'
+
+export const EARTHQUAKE_DEPTH_STOPS = [
+  [0, '#d95d39'],
+  [35, '#ef8a47'],
+  [70, '#f2c14e'],
+  [150, '#73bfb8'],
+  [300, '#4f7cac'],
+  [500, '#5d4b8c'],
+] as const
+
+export function earthquakeDepthColorExpression(): ExpressionSpecification {
+  const stops = EARTHQUAKE_DEPTH_STOPS.flatMap(([depth, color]) => [depth, color])
+
+  return [
+    'case',
+    ['!=', ['get', 'depthKm'], null],
+    [
+      'interpolate',
+      ['linear'],
+      ['to-number', ['get', 'depthKm'], 0],
+      ...stops,
+    ],
+    EARTHQUAKE_DEPTH_UNKNOWN_COLOR,
+  ] as unknown as ExpressionSpecification
+}
